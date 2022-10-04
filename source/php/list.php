e6e4f3daa6a563dfb2c6c8ac412bdc870df58d38
Nyirfa Bálint <78541351+balintnyirfa@users.noreply.github.com>
2022-10-04 20:37:34 +0200

    Négy .php fájl feltöltve
    A footer és header adja a keretét az oldalaknak, a list.php-n történik a viccek kilistázása, az upload.php-n lehet majd vicceket feltölteni.

---
+++ source/php/list.php
@@ -0,0 +1,9 @@
+<?php
+    // Header
+?>
+
+
+
+<?php
+    // Footer
+?>
---
+++ source/php/upload.php
@@ -0,0 +1,15 @@
+<?php
+    // Header
+?>
+
+<div>
+    <form action="./include/upload_inc.php" method="post" enctype="multipart/form-data">
+        Text: <input type="text" name="joke" id="joke"><br>
+        Meme: <input type="file" name="meme" id="meme"><br>
+        <button type="submit">Upload</button>
+    </form>
+</div>
+
+<?php
+    // Footer
+?>
